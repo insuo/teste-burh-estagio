885de36e06225c7a5402b605adac55522452067c
Luis Fernando <luis.fern4ndo@hotmail.com>
2021-01-07 17:22:37 -0300

    listagem de filmes e grid
    > mostrar lista de posters ao pesquisar nome de filme
    > substituir lista de posters ao trocar a palavra chave de pesquisa
    > remover todos os posters caso a palavra chave de pesquisa esteja vazia
    > correções de layout e inicio de responsividade

--- js/search.js
+++ js/search.js
@@ -1,14 +1,51 @@
-var input = document.querySelector('.searchInput');
+var input = document.querySelector(".searchInput");
+var wrapper = document.querySelector(".wrapper");
 
 if (input) input.onchange = handleChange;
 
-function handleChange(e){
+function handleChange(e) {
+  $(".search").removeClass("searching");
+  $(".search").addClass("searching");
+  //substituir o titulo do filme por outro
+  if(e.target.value){
+    let del = document.querySelectorAll(".img");
+  
+    Array.from(del).forEach(index => {
+      index.remove();
+    });
+  }
 
-let url = "http://www.omdbapi.com/?s="+e.target.value+"&apikey=c17838bb";
+  if(!e.target.value){
+    let del = document.querySelectorAll(".img");
+    console.log(del)
+    $(".search").removeClass("searching");
+  
+    Array.from(del).forEach(index => {
+      index.remove();
+    });
+  }
 
-$.get(url, function(data){
-  console.table(data);
-});
+  if (e.target.value != null && $(".search").hasClass("searching")) {
+    let url =
+    "http://www.omdbapi.com/?s=" + e.target.value + "&apikey=c17838bb";
 
-console.log(url);
+    $.getJSON(url, function (json) {
+      // console.table(json);
+      let list = json.Search;
+
+      if(list != undefined){
+        list.forEach(function (item) {
+          if (item.Poster != "N/A") {
+            let image = document.createElement("img");
+            image.classList.add("img");
+            image.src = item.Poster;
+            wrapper.appendChild(image);
+          }
+        });
+      }
+      else{
+        $(".search").removeClass("searching");
+      }
+    });
+  }
 }
